3bd34df2bb74ac0ca587d4bb80e4e74473bc890a
lyndsay.fearnall <lyndsay.fearnall@gmail.com>
2019-04-13 21:34:46 -0400

    change to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,17 +11,17 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options: [{
         path: `${__dirname}/content/blog`,
         name: `blog`,
-      },
+      },],       
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options: [{
         path: `${__dirname}/content/blog2`,
         name: `blog2`,
-      },
+      },],
     },
     {
       resolve: `gatsby-source-filesystem`,
